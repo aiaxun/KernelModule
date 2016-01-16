622c1b6e58a4a6f12ad4961818ddcb305183a97f
aiaxun <qintaoshen@ubuntukylin.com>
2016-01-16 19:50:45 +0800

    normal running

--- .slides.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sqt/modules/KernelModule/slides.o := ld -m elf_x86_64   -r -o /home/sqt/modules/KernelModule/slides.o /home/sqt/modules/KernelModule/slide.o /home/sqt/modules/KernelModule/my_module.o 

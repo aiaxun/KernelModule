41a45942db1710a17b8f55607a2558bb8e9b79de
aiaxun <qintaoshen@ubuntukylin.com>
2016-01-24 10:23:49 +0800

    new files

--- .tmp_versions/trigger.mod
+++
@@ -1,2 +0,0 @@
-/home/kylin/Modules/KernelModule/trigger.ko
-/home/kylin/Modules/KernelModule/mark_page_nx.o /home/kylin/Modules/KernelModule/handle_page_fault.o
--- .trigger.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/kylin/Modules/KernelModule/trigger.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/kylin/Modules/KernelModule/trigger.ko /home/kylin/Modules/KernelModule/trigger.o /home/kylin/Modules/KernelModule/trigger.mod.o
